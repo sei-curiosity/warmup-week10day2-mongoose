866b233475f6e3cc51f1b2d8ed0185ef9154d8bf
samidahlawi <o.t5@hotmail.com>
2020-01-20 03:46:26 +0300

    add warm-up

---
+++ app-student.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+// const dbName = 'student-crud'
+const url = `mongodb://localhost/student-crud`
+mongoose.connect(url,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true 
+    })
+
+const db = mongoose.connection
+
+// import student model
+// ??
+
+
+
+///     CRUD    ///
+
+//>>>
+// create 
+
+//<<< 
+
+
+// index
+//>>>
+
+//<<<
+
+
+//show 
+//>>>
+
+//<<<
+
+
+
+// destroy 
+//>>>
+
+//<<<
+
+// update 
+//>>>
+
+//<<<
---
+++ models/student.js
@@ -0,0 +1,31 @@
+
+//- import the 'mongoose' 
+
+
+
+// The new schema, here
+    //- Create new Schema called 'studentSchema'
+    // * firstName - (String, required)
+    // * lastName - (String, required)
+    // * grade - (Number, required)
+    // * age - (Number, Greater than or equal 18)
+    // * city - (String)
+
+
+
+
+
+
+
+
+
+
+    
+
+
+// The model of the schema 
+
+
+
+
+// Export the model 
